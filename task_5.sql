0251581b08fed0e5d5b43fabe16831d41a5b0f9a
musapeace <117737251+musapeace@users.noreply.github.com>
2025-02-02 13:52:25 +0000

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS alx_book_store.customer (
+CREATE TABLE IF NOT EXISTS customer (
   customer_id INT,
   customer_name VARCHAR(255),
   email VARCHAR(255),
   address VARCHAR(255)
 );
 
-INSERT INTO alx_book_store.customer (customer_id, customer_name, email, address) 
+INSERT INTO customer (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
