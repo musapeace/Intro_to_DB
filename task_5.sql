39e8a2150e81b11fcab51e345b6756c029e9fcb8
musapeace <mubarakinda@gmail.com>
2025-02-02 12:57:13 +0000

    script to insert single in a table

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Inserting a single row into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, customer_address)
+VALUES (1, 'Muhammad Musa', 'mmmusapeace@gmail.com', 'kaduna nigeria.');
